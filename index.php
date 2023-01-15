b3b5bcd17c9ccda4abfeee40472f7600e0ca6d47
Victor C <74551928+victorcamacaro253@users.noreply.github.com>
2023-01-15 14:33:19 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -21,12 +21,25 @@ if (isset($_SESSION['user'])) {
 
     $userform=$_POST['username'];
     $passform=$_POST['password'];
+    $md5pass=md5($passform);
+
+   
 
 
     if ($user->userexists($userform,$passform)) {
     	/*echo "<div style='background:lightgreen;width:20%;color:white;'>
                usuario validado
                </div>";*/
+
+                /*$conexion=new mysqli('localhost','root','','implantacion');
+
+    
+   $stmt=$conexion->query("SELECT * FROM Usuarios WHERE Usuario='$userform' and Password='$md5pass' ");
+               
+                $rs=$stmt->fetch_assoc();
+                $_SESSION['id']=$rs['Id_usuario'];*/
+
+       
     	$usersession->setcurrentuser($userform);
     	$user->setuser($userform);
     	include_once("vistas/home_cliente.php");
